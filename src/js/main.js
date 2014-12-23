6b6fd75d05f0366b5e40b708af0e736c2c0bf85e
Audrey Carlsen <acarlsen@seattletimes.com>
2014-12-23 13:29:22 -0800

    Added icanhaz template, proper dummy data

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 //Use CommonJS style via browserify to load other modules
+
+var ich = require("icanhaz");
+var quizTemplate = require("./_quizTemplate.html");
+ich.addTemplate("quizTemplate", quizTemplate);
+
+document.body.innerHTML = ich.quizTemplate(quizData);

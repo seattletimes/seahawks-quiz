3b176fd68b6f547772ebc745054167436e9c6fab
Audrey Carlsen <acarlsen@seattletimes.com>
2014-12-23 16:31:51 -0800

    Basic answer selection and question navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,30 @@
 //Use CommonJS style via browserify to load other modules
 
+var $ = require("jquery");
 var ich = require("icanhaz");
 var quizTemplate = require("./_quizTemplate.html");
 
 // Set up quiz box template
 ich.addTemplate("quizTemplate", quizTemplate);
-document.querySelector(".quiz-box").innerHTML = ich.quizTemplate(quizData[0]);
+
+var showQuestion = function(index) {
+  $(".question-box").html(ich.quizTemplate(quizData[index]));
+};
+
+var answers = [];
+var questionIndex = 0;
+showQuestion(questionIndex);
+
+$(".next").click(function() {
+  if ($("input:checked")) {
+    answers.push($("input:checked")[0].id);
+
+    if (questionIndex < quizData.length - 1) { 
+      questionIndex += 1; 
+      showQuestion(questionIndex);
+    } else {
+      $(".quiz-box").html(answers);
+    }
+  }
+});
+
